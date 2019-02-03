0dd5f1d5d76b393997bf3aab91277f77ec7f02e8
benche <ben.che9@gmail.com>
2019-02-03 18:25:48 -0500

    adding selection logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.scss';
 
 class App extends Component {
 	state = {
-		counter: 0
+		counter: 0,
+		selected: null
 	};
 
 	componentDidMount() {
@@ -15,10 +16,9 @@ class App extends Component {
 		});
 	}
 
-	renderCards() {
+	renderCards = () => {
 		if (!!this.state.cardList) {
 			return this.state.cardList.map((card) => {
-				console.log(card);
 				return (
 					<Card
 						tag={card.name}
@@ -26,11 +26,38 @@ class App extends Component {
 						solved={card.solved}
 						active={card.active}
 						key={card.id}
+						handleCardCheck={this.handleCardCheck}
 					/>
 				);
 			});
 		}
-	}
+	};
+
+	handleCardCheck = (tagName, index) => {
+		let dataCopy = this.state.cardList;
+		if (this.state.selected === null) {
+			dataCopy[index].active = true;
+			this.setState({
+				selected: index,
+				cardList: dataCopy
+			});
+		} else {
+			if (dataCopy[this.state.selected].name === tagName) {
+				dataCopy[this.state.selected].solved = true;
+				dataCopy[index].solved = true;
+				this.setState({
+					selected: null,
+					cardList: dataCopy
+				});
+			} else {
+				dataCopy[this.state.selected].active = false;
+				this.setState({
+					selected: null,
+					cardList: dataCopy
+				});
+			}
+		}
+	};
 
 	render() {
 		return (
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './Card.scss';
 
 export default function Card(props) {
-	console.log(props);
-	return <div className={`card fas fa-${props.tag}`} />;
+	if (props.solved) {
+		return <div className={`card fas fa-${props.tag} solved`} />;
+	} else if (props.active) {
+		return <div className={`card fas fa-${props.tag} active`} />;
+	} else {
+		return (
+			<div
+				className={`card`}
+				onClick={() => props.handleCardCheck(props.tag, props.id)}
+			/>
+		);
+	}
 }
